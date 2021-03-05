a18aae07b5a698cca05c44d789c76bd7384f289e
Vatsal Jagani <vatsaljagani85@gmail.com>
2021-03-05 20:56:03 +0530

    CY-97: Fixed row expansion issue on the tenable dashboard.

--- cyences_app_for_splunk/appserver/static/cs_tenable.js
+++ cyences_app_for_splunk/appserver/static/cs_tenable.js
@@ -31,14 +31,19 @@ function(TableView, SearchManager, mvc, _){
         render: function($container, rowData) {
             // rowData contains information about the row that is expanded.  We can see the cells, fields, and values
             // We will find the cell values to use its value
-            var allIPs = _(rowData.cells).find(function (cell) {
+            let allIPs = _(rowData.cells).find(function (cell) {
                return cell.field === 'tenable_all_ips';
             }).value;
-            var host = _(rowData.cells).find(function (cell) {
+            if(allIPs == "null"){
+                allIPs = _(rowData.cells).find(function (cell) {
+                    return cell.field === 'ip';
+                }).value;
+            }
+            let host = _(rowData.cells).find(function (cell) {
                 return cell.field === 'host';
             }).value;
             //update the search with the HOST_ID that we are interested in
-            this._searchManager.set({ search: `| inputlookup cs_tenable_vuln | search ip IN (${allIPs}) OR host="${host}" | sort -vul_severity_id, -vul_state
+            this._searchManager.set({ search: `| inputlookup cs_tenable_vuln | search ip IN (${allIPs}) host="${host}" | sort -vul_severity_id, -vul_state
             | table vul_id, vul_name, vul_severity, vul_state, vul_family, vul_has_patch, vul_port, vul_protocol, vul_risk_factor, vul_type, vul_version, vul_description, vul_synopsis, _time, last_fixed, last_found, vul_cpe, vul_in_the_news`});
             // $container is the jquery object where we can put out content.
             // In this case we will render our chart and add it to the $container
