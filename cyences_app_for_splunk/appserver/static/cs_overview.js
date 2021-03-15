ec250d5adcf518d53c2ac313e0dff12292a562b0
Vatsal Jagani <vatsaljagani85@gmail.com>
2021-03-15 15:55:30 +0530

    Added links to overview page for more dashboards.

--- cyences_app_for_splunk/appserver/static/cs_overview.js
+++ cyences_app_for_splunk/appserver/static/cs_overview.js
@@ -12,13 +12,17 @@ require([
         { id: 'o365_reports_link', url: baseURL.concat('cs_o365_reports')},
         { id: 'network_reports_link', url: baseURL.concat('cs_network_reports')},
         { id: 'paf_reports_link', url: baseURL.concat('cs_paloalto_firewall_reports')},
-        { id: 'malicious_reports_link', url: baseURL.concat('cs_malicious_ip_list')},
         { id: 'sophos_reports_link', url: baseURL.concat('cs_sophos_reports')},
-        { id: 'vpn_reports_link', url: baseURL.concat('cs_vpn_reports')},
-        { id: 'auth_reports_link', url: baseURL.concat('cs_authentication_reports')},
         { id: 'windowsdef_reports_link', url: baseURL.concat('cs_windows_defender_reports')},
         { id: 'crowdstrike_reports_link', url: baseURL.concat('cs_crowdstrike_reports')},
+        { id: 'vpn_reports_link', url: baseURL.concat('cs_vpn_reports')},
+        { id: 'auth_reports_link', url: baseURL.concat('cs_authentication_reports')},
         { id: 'admin_reports_link', url: baseURL.concat('cs_splunk_admin_reports')},
+        { id: 'malicious_reports_link', url: baseURL.concat('cs_malicious_ip_list')},
+        { id: 'device_master_table_link', url: baseURL.concat('cs_device_master_table')},
+        { id: 'lansweeper_link', url: baseURL.concat('cs_lansweeper')},
+        { id: 'qualys_link', url: baseURL.concat('cs_qualys')},
+        { id: 'tenable_link', url: baseURL.concat('cs_tenable')},
     ];
 
     // Iterate through the link and add the onClick method
