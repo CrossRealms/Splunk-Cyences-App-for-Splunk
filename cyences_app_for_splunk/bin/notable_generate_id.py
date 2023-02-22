44d0ecd8ac1db2acb34c338ee539c602c6833340
Vatsal Jagani <vatsaljagani85@gmail.com>
2023-02-22 22:22:36 +0530

    CY-388: Added unique notable event id in the event.

---
+++ cyences_app_for_splunk/bin/notable_generate_id.py
@@ -0,0 +1,28 @@
+import sys
+import uuid
+
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration
+
+import logging
+import logger_manager
+
+logger = logger_manager.setup_logging("notable_generate_id_command", logging.INFO)
+
+
+@Configuration(local=True)
+class NotableGenerateId(StreamingCommand):
+
+    def stream(self, records):
+        try:
+            for record in records:
+                record['notable_event_id'] = str(uuid.uuid4())
+                yield record
+
+        except Exception as err:
+            msg = "Error occurred in NotableGenerateId command: {}".format(err)
+            self.write_error(msg)
+            logger.exception(msg)
+
+
+if __name__ == "__main__":
+    dispatch(NotableGenerateId, sys.argv, sys.stdin, sys.stdout, __name__)
