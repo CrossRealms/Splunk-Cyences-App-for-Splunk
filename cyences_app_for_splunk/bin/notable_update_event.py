c09d532032db79fe000145fa3cfbe8852863ee59
Vatsal Jagani <vatsaljagani85@gmail.com>
2023-03-01 19:06:07 +0530

    CY-388: Make the notable event editor command streaming for easy bulk update.

--- cyences_app_for_splunk/bin/notable_update_event.py
+++ cyences_app_for_splunk/bin/notable_update_event.py
@@ -1,6 +1,6 @@
 import sys
 
-from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option, validators
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
 import cs_utils
 from notable_lookup_handler import NotableEventLookupHandler
 
@@ -9,28 +9,30 @@ import logger_manager
 logger = logger_manager.setup_logging('notable_event_update', logging.DEBUG)
 
 
-@Configuration()
-class NotableEventUpdate(GeneratingCommand):
+@Configuration(local=True)
+class NotableEventUpdate(StreamingCommand):
 
     notable_event_id = Option(name="notable_event_id", require=True)
     alert_time = Option(name='alert_time', require=False, default=None)
     assignee = Option(name='assignee', require=False, default=None)
     status = Option(name="status", require=False, default=None)
 
-    def generate(self):
+    def stream(self, records):
         try:
             session_key = cs_utils.GetSessionKey(logger).from_custom_command(self)
 
             user_making_change = self._metadata.searchinfo.username
             nehlh = NotableEventLookupHandler(logger, session_key, user_making_change=user_making_change)
-            response = nehlh.update_entry(self.notable_event_id,
-                                alert_time=self.alert_time,
-                                assignee=self.assignee,
-                                status=self.status)
-            if response:
-                yield {"msg": "Notable event lookup entry updated. notable_event_id={}".format(self.notable_event_id)}
-            else:
-                yield {"error_msg": "Unable to create/update notable event lookup entry. notable_event_id={}".format(self.notable_event_id)}
+
+            for entry in records:
+                response = nehlh.update_entry(entry.notable_event_id,
+                                    alert_time=entry.alert_time,
+                                    assignee=entry.assignee,
+                                    status=entry.status)
+                if response:
+                    yield {"success_msg": "Notable event lookup entry updated.", "notable_event_id": entry.notable_event_id}
+                else:
+                    yield {"error_msg": "Unable to create/update notable event lookup entry.", "notable_event_id": entry.notable_event_id}
         except Exception as e:
             logger.exception("Error in NotableEventUpdate command: {}".format(e))
             raise e
