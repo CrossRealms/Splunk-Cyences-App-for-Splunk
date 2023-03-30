7fdd7234753f0185023a9ba06b162caa84156685
Mahir Chavda <mahir.chavda13@gmail.com>
2023-03-30 15:26:10 +0530

    simplify overview dashobard links logic

--- cyences_app_for_splunk/appserver/static/cs_overview.js
+++ cyences_app_for_splunk/appserver/static/cs_overview.js
@@ -9,36 +9,10 @@ require([
     'use strict';
     let baseURL = window.location.href.split('cs_overview')[0];
 
-    // List of report links
-    let report_links = [
-        { id: 'ad_reports_link', url: baseURL.concat('cs_ad_reports')},
-        { id: 'windows_reports_link', url: baseURL.concat('cs_windows_reports')},
-        { id: 'windows_patch_link', url: baseURL.concat('cs_windows_patch')},
-        { id: 'linux_reports_link', url: baseURL.concat('cs_linux_reports')},
-        { id: 'o365_reports_link', url: baseURL.concat('cs_o365_reports')},
-        { id: 'aws_reports_link', url: baseURL.concat('cs_aws_user_activity')},
-        { id: 'gsuite_reports_link', url: baseURL.concat('cs_gsuite_reports')},
-        { id: 'network_reports_link', url: baseURL.concat('cs_network_reports')},
-        { id: 'dns_tracker_link', url: baseURL.concat('cs_dns_tracker')},
-        { id: 'paf_reports_link', url: baseURL.concat('cs_paloalto_firewall_reports')},
-        { id: 'sophos_firewall_link', url: baseURL.concat('cs_sophos_firewall')},
-        { id: 'sophos_reports_link', url: baseURL.concat('cs_sophos_reports')},
-        { id: 'windowsdef_reports_link', url: baseURL.concat('cs_windows_defender_reports')},
-        { id: 'o365_defender_atp_link', url: baseURL.concat('cs_o365_defender_atp')},
-        { id: 'crowdstrike_reports_link', url: baseURL.concat('cs_crowdstrike_reports')},
-        { id: 'kaspersky_reports_link', url: baseURL.concat('cs_kaspersky_reports')},
-        { id: 'vpn_reports_link', url: baseURL.concat('cs_vpn_reports')},
-        { id: 'auth_reports_link', url: baseURL.concat('cs_authentication_reports')},
-        { id: 'malicious_reports_link', url: baseURL.concat('cs_malicious_ip_list')},
-        { id: 'device_inventory_table_link', url: baseURL.concat('cs_device_inventory_table')},
-        { id: 'lansweeper_link', url: baseURL.concat('cs_lansweeper')},
-        { id: 'vulnerability_link', url: baseURL.concat('cs_vulnerability')},
-    ];
-
     // Iterate through the link and add the onClick method
-    $.each(report_links, function(index, report){
-        $(`#${report.id}`).on("click", function(){
-            window.open(report.url, "_blank");
+    $('#cyences_links').children().each(function (index, element) {
+        $(this).on("click", function(){
+            window.open(baseURL.concat(element.id), "_blank");
         });
     });
 
