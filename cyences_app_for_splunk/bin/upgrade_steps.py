b2ac4c9f6a8d433e444a3a312013e8343c21c2fc
Mahir Chavda <mahir.chavda13@gmail.com>
2023-05-05 22:00:35 +0530

    resolve review comments

--- cyences_app_for_splunk/bin/upgrade_steps.py
+++ cyences_app_for_splunk/bin/upgrade_steps.py
@@ -2,14 +2,18 @@ import time
 import cs_utils
 
 import splunklib.client as client
-from splunklib.results import JSONResultsReader
+import splunklib.results as results
 
 
-def pretty(response, logger):
-    reader = JSONResultsReader(response)
+def handle_results(response, logger):
+    reader = results.JSONResultsReader(response)
+    count = 0
     for result in reader:
+        if isinstance(result, results.Message):
+            logger.warning("{}: {}".format(result.type, result.message))
         if isinstance(result, dict):
-            logger.info(result)
+            count += 1
+    logger.info("Total {} rows".format(count))
 
 
 def upgrade_4_0_0(session_key, logger):
@@ -19,12 +23,15 @@ def upgrade_4_0_0(session_key, logger):
 
     logger.info("Running Login Location Lookup Gen search for -12w to -8w timerange")
     response = service.jobs.oneshot(SEARCH_QUERY, output_mode="json", earliest_time='-12w@w', latest_time='-8w@w')
+    handle_results(response, logger)
     time.sleep(60)
     logger.info("Running Login Location Lookup Gen search  for -8w to -4w timerange")
     response = service.jobs.oneshot(SEARCH_QUERY, output_mode="json", earliest_time='-8w@w', latest_time='-4w@w')
+    handle_results(response, logger)
     time.sleep(60)
     logger.info("Running Login Location Lookup Gen search  for -4w to w timerange")
     response = service.jobs.oneshot(SEARCH_QUERY, output_mode="json", earliest_time='-4w@w', latest_time='@w')
+    handle_results(response, logger)
 
 
 version_upgrade = (
