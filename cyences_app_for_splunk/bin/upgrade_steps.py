d7e6e8cc717222670afbe60bc4e7ab10b17701ac
Mahir Chavda <mahir.chavda13@gmail.com>
2023-05-05 18:34:32 +0530

    custom upgrade steps functionality

---
+++ cyences_app_for_splunk/bin/upgrade_steps.py
@@ -0,0 +1,33 @@
+import time
+import cs_utils
+
+import splunklib.client as client
+from splunklib.results import JSONResultsReader
+
+
+def pretty(response, logger):
+    reader = JSONResultsReader(response)
+    for result in reader:
+        if isinstance(result, dict):
+            logger.info(result)
+
+
+def upgrade_4_0_0(session_key, logger):
+    SEARCH_QUERY = '| savedsearch "Authentication - Usual Login Location Lookup Gen"'
+
+    service = client.connect(token=session_key, app=cs_utils.APP_NAME)
+
+    logger.info("Running Login Location Lookup Gen search for -12w to -8w timerange")
+    response = service.jobs.oneshot(SEARCH_QUERY, output_mode="json", earliest_time='-12w@w', latest_time='-8w@w')
+    time.sleep(60)
+    logger.info("Running Login Location Lookup Gen search  for -8w to -4w timerange")
+    response = service.jobs.oneshot(SEARCH_QUERY, output_mode="json", earliest_time='-8w@w', latest_time='-4w@w')
+    time.sleep(60)
+    logger.info("Running Login Location Lookup Gen search  for -4w to w timerange")
+    response = service.jobs.oneshot(SEARCH_QUERY, output_mode="json", earliest_time='-4w@w', latest_time='@w')
+
+
+version_upgrade = (
+    ('3.1.0', None),
+    ('4.0.0', upgrade_4_0_0),
+)
