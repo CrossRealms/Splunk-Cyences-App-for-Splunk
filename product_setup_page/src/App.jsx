06a4dd9d473f42c8fce1edfb23028971b4beb3f4
Hardik Dholariya <hardikdholariya1@gmail.com>
2023-11-30 17:02:15 +0530

    Generated the configuration page with latest changes

--- product_setup_page/src/App.jsx
+++ product_setup_page/src/App.jsx
@@ -14,7 +14,7 @@ const TABS = [
     'Macro Setup',
     'HoneyDB Configuration',
     'MaliciousIP Collector Configuration',
-    'Sophos Endpoint Protection API Configuration',
+    'Sophos Central API Configuration',
     'Cyences Email Action Configuration',
     'Device Inventory Configuration'
 ]
