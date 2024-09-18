180a2b8648c777e184dc0194b5bdb24ecaa325cf
Vatsal Jagani <vatsaljagani85@gmail.com>
2024-09-18 16:37:23 +0530

    Bug fix on the overview dashboard and on the custom command of mvintersection.

--- cyences_app_for_splunk/bin/cyences_mvintersection.py
+++ cyences_app_for_splunk/bin/cyences_mvintersection.py
@@ -16,13 +16,22 @@ class CyencesMVIntersection(StreamingCommand):
 
 
     def stream(self, records):
+        logger.info("Starting CyencesMVIntersection command.")
         try:
             for record in records:
-                set1 = set(record.get(self.set1, []))
-                set2 = set(record.get(self.set2, []))
+                set1 = record.get(self.set1, [])
+                if type(set1) == list:
+                    set1 = set(set1)
+                else:
+                    set1 = set([set1])
+                set2 = record.get(self.set2, [])
+                if type(set2) == list:
+                    set2 = set(set2)
+                else:
+                    set2 = set([set2])
 
                 record["intersection"] = list(set1.intersection(set2))
-                logger.debug("intersection={}".format(record['intersection']))
+                logger.debug(f"set1={set1} & set2={set2} -> intersection={record['intersection']}")
                 yield record
 
         except Exception as err:
