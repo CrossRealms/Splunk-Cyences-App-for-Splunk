07015b57c4b7bf8f115c701187e2a05f676d804c
crmpandya <mpandya@crossrealms.com>
2025-07-30 14:55:42 +0530

    Added macro for threshold configuration.

--- product_setup_page/src/allMacros.js
+++ product_setup_page/src/allMacros.js
@@ -51,6 +51,7 @@ let allMacros = [
             { name: 'cs_ad_important_policy', description: 'List of important policies. (e.g. "val1","val2")' },
             { name: 'cs_ad_important_user', description: 'List of important users. (e.g. "val1","val2")' },
             { name: 'cs_ad_important_group', description: 'List of important groups. (e.g. "val1","val2")' },
+            { name: 'cs_windows_failed_logins_by_user_threshold' },
         ]
     },
     {
