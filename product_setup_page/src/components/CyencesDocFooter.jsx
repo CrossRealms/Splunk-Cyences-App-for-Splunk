e0e1ca7c366ae169c30179b31a1c5dbbfd9f2fde
cr-mpandya <mpandya@crossrealms.com>
2026-03-20 16:16:49 +0530

    Deleted product_setup_page folder.

--- product_setup_page/src/components/CyencesDocFooter.jsx
+++
@@ -1,9 +0,0 @@
-import Link from '@splunk/react-ui/Link';
-
-export default function CyencesDocFooter({ location }) {
-    return (
-        <footer style={{ marginTop: '100px' }}>
-            <Link style={{ marginLeft: '20px' }} to={`https://cyences.com/${location}`} openInNewContext>Configuration Documentation</Link>
-        </footer>
-    );
-}
